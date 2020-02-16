046952ebe4294ef85ad70db6a6ae4f1e828c86e3
pramod sareddy <reddypramod85>
2020-02-16 17:16:35 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Box, Button, Heading, Grommet } from "grommet";
+import { Notification } from "grommet-icons";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6"
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px"
+    }
+  }
+};
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
+function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <Grommet theme={theme} full>
+      <Box fill>
+        <AppBar>
+          <Heading level="3" margin="none">
+            My App
+          </Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => setShowSidebar(!showSidebar)}
+          />
+        </AppBar>
+        <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+          <Box flex align="center" justify="center">
+            app body
+          </Box>
+          {showSidebar && (
+            <Box
+              width="medium"
+              background="light-2"
+              elevation="small"
+              align="center"
+              justify="center"
+            >
+              sidebar
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </Grommet>
+  );
+}
+
+export default App;
